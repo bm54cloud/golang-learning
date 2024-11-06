c864094874a60f93c3c38bed1adc8b13410474e8
Brandi McCall <brandi@defenseunicorns.com>
2024-11-05 20:19:06 -0700

    update notes and examples

--- booking-app/main.go
+++ booking-app/main.go
@@ -1,23 +1,133 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // Use Println with variables and constants
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	// conferenceTickets is a constant instead of a variable because it does not change throughout the application
 	const conferenceTickets = 50
 	// Keep track of ticket count to know how many are still available
-	var remainingTickets = 50
+	// Explicitely define uint as the data type so that it does not accept negative numbers
+	var remainingTickets uint = 50
 
 	fmt.Println("Welcome to the", conferenceName, "booking application!")
 	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
-	fmt.Println("Get your tickets here to attend")
+	fmt.Println("Get your tickets here to attend.")
 
-	// Printf is used for printing formatted data
+	// Printf is used for printing formatted data (%v, %s, etc.)
 	// Use \n at the end to escape to a new line (Println automatically adds a new line, but Printf does not)
 	// Reference the variable to be inserted at the %v spot at the end of the quotation
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+	fmt.Println("Get your tickets here to attend.")
+
+	// Use %T to print the data type
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	// Ask for user input
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter the number of tickets you want to book: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	// Store a list of the inputted user names via an array of string type
+	var bookings = [50]string{}
+	bookings[0] = firstName + " " + lastName
+
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("The type of the array: %T\n", bookings)
+	fmt.Printf("The size of the array: %v\n", len(bookings))
+
+	// Example of how to do bookings as a slice
+	var bookings2 []string
+	bookings2 = append(bookings2, firstName+" "+lastName)
+
+	fmt.Printf("The whole slice: %v\n", bookings2)
+	fmt.Printf("The first value: %v\n", bookings2[0])
+	fmt.Printf("The type of the slice: %T\n", bookings2)
+	fmt.Printf("The size of the slice: %v\n", len(bookings2))
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("There are %v tickets remaining for the %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all our bookings: %v\n", bookings2)
+
+	forLoop()
+}
+
+// Use for loop to allow booking over and over again
+func forLoop() {
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+
+	// Declare the bookings2 slice
+	var bookings2 []string
+
+	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend.")
+
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		// Ask for user input
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter the number of tickets you want to book: ")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+
+		// Example of how to do bookings as a slice
+		bookings2 = append(bookings2, firstName+" "+lastName)
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("There are %v tickets remaining for the %v\n", remainingTickets, conferenceName)
+
+		// Declare slice to only print firstName for privacy
+		firstNames := []string{}
+		for _, booking := range bookings2 {
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("These first names of bookings are: %v\n", firstNames)
+
+		// Check if there are any tickets left
+		if remainingTickets == 0 {
+			// End the program (for loop)
+			fmt.Println("Our conference is booked out. Please come back next year.")
+			break
+		}
+	}
 }
--- notes.md
+++ notes.md
@@ -7,16 +7,18 @@
 - Must declare an entrypoint for your application
 - A function can only have 1 "main" function, because you can only have 1 entrypoint
 - Must **import** any functions/libraries you are going to use
-<img src="images/package.png" width="200" height="100">
+<img src="booking-app/images/package.png" width="200" height="100">
 - Run `go run main.go` to execute the file
 
 **Variables** 
 - Golang variables are mutable (can change) after they are declared
 - Use `var` to declare them
-    ```
-    var x int = 10
-    x = 20 // Allowed
-    ```
+    `var x int = 10`
+- Can also use `:=` to declare them (cannot use this for constants or if you want to explicitly define a type)
+    `x := 10`
+- Use `=` for assigning values to variables
+- Use `==` for comparing of 2 values
+
 **Constants** 
 - Golang constants cannot be changed once they are declared
 - Use `const` to declare them
@@ -24,4 +26,61 @@
     const pi = 3.14
     pi = 3.14159 // Not allowed
     ```
+**Date Types**
+- In Golang, all values have data types
+- Each language has different data types that it supports
+- When declaring a variable, you need to tell the Go Compiler the data type
+- ex. `var userName string` indicates a string type, just like `var userName = "Tom"` indicates a string type
+- ex. `var userTickets int` indicates an integer type, just like `var userTickets = 34` indicated an integer type
+- Optionally, you may explicitly define the data types in the variable
+    - Ex. `var conferenceName string = "Go Conference"` or `const conferenceTickets int = 50`
+- Integers can be whole numbers, decimals, or negative numbers, so you may want to explicitly define the data type when using these
+- Floating point types can contain a decimal component
+
+**User Input**
+- Can use fmt.Scan() to collect user input data and read formatted input
+- Must create a pointer to save the variable in
+- When defining variables, the variable value gets saved in memory on your computer
+- A **pointer** (or special variable) is a variable that points to the memory address of another variable
+- Use `&` before a variable to print the memory location of that variable
+    - Ex. `fmt.Println(&remainingTickets)` would print the memory location (ex. 0xc00014098)
+
+**Number Compilations**
+- When doing compilations with numbers, they must be of the same type
+- Ex. Cannot do a - b if a is type `int` and b is type `uint`
+
+**Arrays**
+- When tracking multiple users' data, you want to save that in a list
+- Arrays in go have a fixed size
+- Use square [] brackets to define an array
+- The size of the array is defined in the square brackets
+- Ex. `var bookings = [50]string{}`
+- Define the data type that the array will contain
+- Each array can contain only one data type
+
+**Slices**
+- What if you don't know the size of the array when you create it?
+    - Create a list/array where you don't need to specify the size at the beginning
+    - The array should automatically expand when new elements are added
+- A **slice** is an abstraction of an array and is a more dynamic array
+- Slices can have variable length or get a sub-array of its own
+- Slices are index-based and have a size, but is resized when needed
+- Ex. `var bookings []string`
+- Ex. `var bookings = []string{}`
+- Ex. `bookings := []string{}`
+
+**For Loops**
+- Go only has the `for` loop
+- Declare variables inside a function but outside the for loop that you don't want to be reset by the loop
+- Iterate through a list
+    - Ex. `for index, booking := range bookings
+    - Range interates over elements for different data structures
+- strings.Fields() splits a string with white space as a separator
+    - Ex. Splits string "Nchole Smith" to slice ["Nicole", "Smith"]
+- Underscores `_` are blank identifiers that are used when you want to ignore a variable you don't want to use
+- Use a **break** statement to terminate the for loop. Once the for loop is broken, the application continues with the code right after the break (if there is any more code)
+
+
+
+
 
